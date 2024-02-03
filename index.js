9e01199d516af94bb6951c1c30f8a0ff1761b7d1
fractial <resu.ijaix@gmail.com>
2024-02-03 12:02:42 +0100

    Update

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-const { existsSync, writeFileSync } = require('fs');
+const fs = require('fs');
+const { homedir } = require('os');
 const path = require('path');
-const { add, remove, modify, run } = require('./script');
+const { add } = require('./add');
+const { remove } = require('./remove');
+const { run } = require('./run');
 
-const filePath = path.join(__dirname, '.cycles.json');
+const filePath = path.join(homedir(), '.cycles.json');
 
-if (!existsSync(filePath)) {
-  const jsonData = {
-    configurations: [],
-  };
-
-  writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+if (!fs.existsSync(filePath)) {
+  try {
+    const jsonData = {
+      configurations: [],
+    };
+    fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2));
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-  console.log(
-    'Usage: node index.js <command> [-t <type>] [-n <name>] [-s <scripts>]'
-  );
-  console.log('Commands: add, remove, modify, run');
+  console.log('Usage: cycles <command> [-t <type>] [-n <name>] [-s <scripts>]');
+  console.log('Commands: add, remove, run');
   process.exit(1);
 }
 
 const command = args[0];
 
-const getFlagValue = (flag) => {
-  const index = args.indexOf(flag);
-  return index !== -1 && index + 1 < args.length ? args[index + 1] : null;
+const getFlagValue = (flags) => {
+  for (const flag of flags) {
+    const index = args.indexOf(flag);
+    const value =
+      index !== -1 && index + 1 < args.length ? args[index + 1] : null;
+    if (value !== null) return value;
+  }
 };
 
-let type, name, scripts;
+let name, type, script;
 
 switch (command) {
   case 'add':
-    type = getFlagValue('-t') || getFlagValue('--type');
-    name = getFlagValue('-n') || getFlagValue('--name');
-    scripts =
-      getFlagValue('-s') ||
-      getFlagValue('--script') ||
-      getFlagValue('--scripts');
-    add(type, name, scripts);
+    name = getFlagValue(['-n', '--name']);
+    type = getFlagValue(['-t', '--type']);
+    script = getFlagValue(['-s', '--script', '--scripts']);
+    add(name, type, script);
     break;
   case 'remove':
-    name = args[1];
-    remove(name);
-    break;
-  case 'modify':
+    remove(args[1]);
     break;
   case 'run':
-    name = args[1];
-    run(name);
+    run(args[1]);
     break;
   default:
-    name = args[0];
-    run(name);
+    run(args[0]);
 }
